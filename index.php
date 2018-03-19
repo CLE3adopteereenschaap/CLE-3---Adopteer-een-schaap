c6c2c78c4d03de53dfe0770b5f2ec0aa4d8761ad
janesssa <34273285+janesssa@users.noreply.github.com>
2018-03-19 10:55:48 +0100

    Index
    Begin van de index

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
+require_once 'database.php';
 
+
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form method="post" action="">
+    <input type="text" name="first" required><br>
+    <input type="text" name="last" required><br>
+    <input type="password" name="pwd" required><br>
+    <button type="submit" name="submit">Inloggen!</button>
+</form>
+
+</body>
+</html>
